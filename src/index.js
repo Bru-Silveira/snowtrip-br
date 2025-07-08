b6b002ace644f0348dc4372b5464a550ff3449d5
Nara Sakamoto <nara.sakamoto@gmail.com>
2025-07-08 17:26:56 -0300

    Adicionando o CSS do template Luxex

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import './index.css';
 import App from './App.jsx';
 import Reserva from './Reserva'; // <-- novo componente que você ainda vai criar
+import Detalhes from './Detalhes.jsx';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,6 +13,7 @@ root.render(
       <Routes>
         <Route path="/" element={<App />} />
         <Route path="/reserva/:id" element={<Reserva />} />
+        <Route path="/detalhes/:id" element={<Detalhes />} />
       </Routes>
     </BrowserRouter>
   </React.StrictMode>
